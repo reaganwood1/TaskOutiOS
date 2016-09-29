759229381f48df88ebf70fc6672e21c68f9d688e
Reagan Wood <reaganwood1@gmail.com>
2016-09-28 22:42:18 -0500

    Added code to implement network api to Flickr to display mountain photo. Everything working correctly. Submitting the project for a final review

--- TaskOut/BasicInformationViewController.swift
+++ TaskOut/BasicInformationViewController.swift
@@ -17,22 +17,61 @@ class BasicInformationViewController: UIViewController {
     @IBOutlet weak var returnButton: UIButton!
     
     override func viewDidLoad() {
-        
-        let photoCreator = FlickrPictureConvenience.sharedInstance()
-        activityIndicator.startAnimating()
-        activityIndicator.hidden = false
-        
-        photoCreator.getFlickrImage { (data) in
-            
-            dispatch_async(dispatch_get_main_queue(), {()-> Void in
-                self.activityIndicator.stopAnimating()
-                self.activityIndicator.hidden = true
-                self.imageView.image = UIImage(data: data)
-            }) // end image main queue completion handler
-        }
 
+        let photoCreator = FlickrClient.sharedInstance()
+        if (Reachability.isConnectedToNetwork()) {
+            activityIndicator.startAnimating()
+            activityIndicator.hidden = false
+            
+            // get photo to display in imageView to display in the view
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)) { () -> Void in
+                photoCreator.retrievePhotoFromFlickr("Mountain") { (success, photoURLs, error) in
+                    if (success == true) {
+                        photoCreator.getPhotoImage(photoURLs[0], completionHandler: { (image, success) in
+                            
+                            if (success == true) { // if success, display the photo image
+                                dispatch_async(dispatch_get_main_queue(), {()-> Void in
+                                    self.activityIndicator.stopAnimating()
+                                    self.activityIndicator.hidden = true
+                                    self.imageView.image = UIImage(data: image!)
+                                }) // end image main queue completion handler
+                            } else { // handle network failure
+                                self.displayEmptyAlert("", message: "The photo was not retrieved correctly from the network", actionTitle: "Ok")
+                            }
+                            
+                        })
+                    } else { // handle network failure
+                         self.displayEmptyAlert("", message: "The photo was not retrieved correctly from the network", actionTitle: "Ok")
+                    }
+                    
+                }
+                
+                
+            } // end closure
+        } else {
+            displayEmptyAlert("", message: "It appears that you aren't connected to the network.", actionTitle: "Ok")
+            activityIndicator.hidden = true
+            activityIndicator.stopAnimating()
+        }
     }
     
+    
+    // function for handling alerts of different types
+    func displayEmptyAlert(headTitle: String?, message: String?, actionTitle: String?){
+        
+        // run the alert in the main queue because it's a member of UIKit
+        dispatch_async(dispatch_get_main_queue(), {()-> Void in
+            
+            let alert = UIAlertController(title: headTitle, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: actionTitle, style: .Default, handler: { action in
+                
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }) // end image main queue completion handler
+    }
+
+    // dismiss the view on return button being pressed
     @IBAction func ReturnButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true) { 
             
---
+++ TaskOut/FlickrConvenience.swift
@@ -0,0 +1,90 @@
+//
+//  FlickrConvenience.swift
+//  VirtualTourist
+//
+//  Created by Reagan Wood on 5/31/16.
+//  Copyright © 2016 RW Software. All rights reserved.
+//
+
+import Foundation
+
+extension FlickrClient {
+    
+    // retrieves a single photo from Flickr with specified text search query
+    func retrievePhotoFromFlickr(text: String, completionHandler: (success: Bool, photoURLs: [String], error: String?) -> Void) {
+        
+        var photoURLArray = [String]()
+        
+        let methodParameters: [String:AnyObject] = [FlickrParameterKeys.Method: FlickParameterValues.GetPhotosMethod, FlickrParameterKeys.APIKey: FlickParameterValues.APIKey, FlickrParameterKeys.Extras: FlickParameterValues.MediumURL, FlickrParameterKeys.ResponseFormat: FlickParameterValues.ResponseFormat, FlickrParameterKeys.NoJSONCallback: FlickParameterValues.JSONCallback, FlickParameterValues.Text: text]
+        
+        taskForGetMethod(methodParameters) { (result, error) in
+            
+            if error == nil {
+                //print(result)
+                if let photoDictionary = result[FlickrJSONResponseKeys.Photos] as? [String:AnyObject]{
+                    if let photoCountString = photoDictionary[FlickrJSONResponseKeys.TotalPages] as? String {
+                        if let photoCountInt = Int(photoCountString) {
+                            if photoCountInt > 0 {
+                                if let photosDictionary = photoDictionary[FlickrJSONResponseKeys.Photo] as? [[String:AnyObject]]{
+                                    
+                                    var i: Int = 0
+                                    
+                                    while (i < photosDictionary.count && i < 1) {
+                                        
+                                        var j: Int = 0
+                                        j = Int(arc4random_uniform(UInt32(photosDictionary.count)))
+                                        
+                                        let photoInfo = photosDictionary[j] as [String:AnyObject]
+                                        if let photoURL = photoInfo[FlickrJSONResponseKeys.MediumURL] as? String {
+                                            photoURLArray.append(photoURL)
+                                        } else {
+                                            completionHandler(success: false, photoURLs: photoURLArray, error: nil)
+                                        }
+                                        i += 1
+                                    }
+                                    
+                                    completionHandler(success: true, photoURLs: photoURLArray, error: "no error")
+                                    
+                                } else {
+                                    completionHandler(success: false, photoURLs: photoURLArray, error: "not photos from dictionary could be retrieved")
+                                }
+                            } else {
+                                completionHandler(success: false, photoURLs: photoURLArray, error: "photoCount was less than or equal to 0")
+                            }
+                        } else {
+                            completionHandler(success: false, photoURLs: photoURLArray, error: "photoCount not available")
+                        }
+                    } else {
+                        completionHandler(success: false, photoURLs: photoURLArray, error: "photoCount not available")
+                    }
+                } else {
+                    completionHandler(success: false, photoURLs: photoURLArray, error: "could not parse into photos array")
+                }
+            } else {
+                completionHandler(success: false, photoURLs: photoURLArray, error: "Could not get photo URLs")
+            }
+            
+            
+        }
+    }
+    
+    func getPhotoImage (url: String, completionHandler: (image: NSData?, success: Bool) -> Void) {
+        
+        let nsURL = NSURL(string: url)
+        if let imageData = NSData(contentsOfURL: nsURL!){
+            completionHandler(image: imageData, success: true)
+        } else {
+            completionHandler(image: nil, success: false)
+        }
+    } // end function
+    
+    private func bboxString(latitude: Double, longitude: Double) -> String {
+        // ensure bbox is bounded by minimum and maximums
+        
+        let minimumLon = max(longitude - FlickrConstants.SearchBBoxHalfWidth, FlickrConstants.SearchLonRange.0)
+        let minimumLat = max(latitude - FlickrConstants.SearchBBoxHalfHeight,FlickrConstants.SearchLatRange.0)
+        let maximumLon = min(longitude + FlickrConstants.SearchBBoxHalfWidth, FlickrConstants.SearchLonRange.1)
+        let maximumLat = min(latitude + FlickrConstants.SearchBBoxHalfHeight, FlickrConstants.SearchLatRange.1)
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    }
+}
